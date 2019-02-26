0b23f956ea719b6ecac1f3e092df6cce27a444a3
Roman Makeev <gladez@bk.ru>
2019-02-26 22:35:13 +0400

    1.3.1

---
+++ main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  vgdh
+//
+//  Created by Роман Макеев on 26/02/2019.
+//  Copyright © 2019 Роман Макеев. All rights reserved.
+//
+
+#include "std.hpp"
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, const char * argv[]) {
+    
+    
+    return 0;
+}
---
+++ std.hpp
@@ -0,0 +1,14 @@
+//
+//  main.hpp
+//  vgdh
+//
+//  Created by Роман Макеев on 26/02/2019.
+//  Copyright © 2019 Роман Макеев. All rights reserved.
+//
+
+#ifndef main_hpp
+#define main_hpp
+
+#include <stdio.h>
+
+#endif /* main_hpp */
